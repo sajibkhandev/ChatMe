4d84ff20121e6f1125338937b5a9d619937c4c91
sajibkhandev <sajibkhan.mern@gmail.com>
2025-07-07 21:57:29 +0600

    add forget password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ import TextField from '@mui/material/TextField';
 import { styled } from '@mui/material/styles';
 import Button from '@mui/material/Button';
 import { Link, useNavigate } from 'react-router-dom';
-import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "firebase/auth";
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 import { ToastContainer, toast } from 'react-toastify';
 import { Bars } from 'react-loader-spinner';
@@ -46,11 +46,15 @@ const Login = () => {
   let [showpass, setShowPass] = useState(false)
   let [loader, setLoader] = useState(false)
 
+  let [forgetui, setForgetUI] = useState(false)
+
   let [email, setEmail] = useState("")
   let [password, setPassword] = useState("")
+  let [forgetemail, setforgetEmail] = useState("")
 
   let [emailerror, setEmailError] = useState("")
   let [passworderror, setPasswordError] = useState("")
+  let [forgetemailerror, setforgetEmailError] = useState("")
 
   let handleEmail = (e) => {
     setEmail(e.target.value);
@@ -61,6 +65,11 @@ const Login = () => {
   let handlePassword = (e) => {
     setPassword(e.target.value);
     setPasswordError("")
+
+  }
+  let handleForgetEmail = (e) => {
+    setforgetEmail(e.target.value);
+    setforgetEmailError("")
 
   }
 
@@ -81,21 +90,30 @@ const Login = () => {
 
     if (email && (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) && password) {
 
-     setLoader(true)
+      setLoader(true)
       signInWithEmailAndPassword(auth, email, password)
         .then((user) => {
           if (user.user.emailVerified) {
             toast.success("login done");
             navigate("/home")
             setLoader(false)
+            setLoader(false)
           } else {
             toast.error("Verify Your Email")
+            setLoader(false)
           }
         })
         .catch((error) => {
           const errorCode = error.code;
+          console.log(errorCode);
+          
           if (errorCode.includes("auth/invalid-credential")) {
             toast.error("Give a Right Email and Password");
+            setLoader(false)
+
+          }else if(errorCode.includes("auth/too-many-requests")){
+            toast.error("Try 30 secode Later");
+            setLoader(false)
 
           }
 
@@ -124,74 +142,147 @@ const Login = () => {
 
 
   }
+
+  let handleForgetPassword = () => {
+    setForgetUI(true)
+
+  }
+  let handleBackToLogin = () => {
+    setForgetUI(false)
+
+  }
+
+  let handleSand = () => {
+    if (!forgetemail) {
+      setforgetEmailError("give a emaill")
+
+    } else {
+      sendPasswordResetEmail(auth, forgetemail)
+        .then(() => {
+         toast.success("Check Your Email for Password");
+         setTimeout(()=>{
+          setForgetUI(false)
+
+         },2000)
+         
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          console.log(errorCode);
+          if(errorCode.includes("auth/too-many-requests")){
+           toast.error("Try Later");
+
+          }
+          
+        });
+
+    }
+  }
+
+
+
   return (
-    <Grid container >
-      <Grid size={6}>
-        <div className='reg-content-box'>
-          <div className='reg-content'>
-            <h2>Login to your account!</h2>
-            <div onClick={handleGoogle} className='logo-box'>
-              <img src={GoogleLogo} alt="" />
-              <p>Login with Google</p>
-            </div>
+    <>
+      {
+        forgetui
+          ? <div className='new-ui-for-forgetpassword'>
             <ToastContainer
-              position="top-center"
-              autoClose={5000}
-              hideProgressBar={false}
-              newestOnTop={false}
-              closeOnClick={false}
-              rtl={false}
-              pauseOnFocusLoss
-              draggable
-              pauseOnHover
-              theme="light"
-            />
-            <MyInput onChange={handleEmail} value={email} className='sajibkhan' id="outlined-basic" label="Email Address" variant="outlined" />
-            {
-              emailerror && <p className='error-message'>{emailerror}</p>
-            }
-            <div className='password-input'>
-              <MyInput value={password} onChange={handlePassword} type={showpass ? "text" : "password"} id="outlined-basic" label="Password" variant="outlined" />
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick={false}
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                  />
+            <div className='forgetpassword-box'>
+              <MyInput onChange={handleForgetEmail} value={forgetemail} className='sajibkhan' id="outlined-basic" label="Email Address" variant="outlined" />
               {
-                passworderror && <p className='error-message'>{passworderror}</p>
+                forgetemailerror && <p className='error-message'>{forgetemailerror}</p>
               }
-              <div onClick={() => setShowPass(!showpass)} className='icon-box'>
-                {
-                  showpass
-                    ?
-                    <FiEye />
-                    :
-                    <FiEyeOff />
-                }
+              <div className='button-box'>
+                <MyButton onClick={handleBackToLogin} variant="contained">Back to Login</MyButton>
+                <MyButton onClick={handleSand} variant="contained">Sand</MyButton>
+              </div>
+            </div>
+          </div>
+
+
+
+          :
+          <Grid container >
+            <Grid size={6}>
+              <div className='reg-content-box'>
+                <div className='reg-content'>
+                  <h2>Login to your account!</h2>
+                  <div onClick={handleGoogle} className='logo-box'>
+                    <img src={GoogleLogo} alt="" />
+                    <p>Login with Google</p>
+                  </div>
+                  <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick={false}
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                  />
+                  <MyInput onChange={handleEmail} value={email} className='sajibkhan' id="outlined-basic" label="Email Address" variant="outlined" />
+                  {
+                    emailerror && <p className='error-message'>{emailerror}</p>
+                  }
+                  <div className='password-input'>
+                    <MyInput value={password} onChange={handlePassword} type={showpass ? "text" : "password"} id="outlined-basic" label="Password" variant="outlined" />
+                    {
+                      passworderror && <p className='error-message'>{passworderror}</p>
+                    }
+                    <div onClick={() => setShowPass(!showpass)} className='icon-box'>
+                      {
+                        showpass
+                          ?
+                          <FiEye />
+                          :
+                          <FiEyeOff />
+                      }
+                    </div>
+
+                  </div>
+                  {
+                    loader
+                      ? <Bars
+                        height="80"
+                        width="80"
+                        color="#4fa94d"
+                        ariaLabel="bars-loading"
+                        wrapperStyle={{}}
+                        wrapperClass=""
+                        visible={true}
+                      />
+                      :
+                      <MyButton onClick={handleLogin} variant="contained">Login to Continue</MyButton>
+
+                  }
+                  <p>Don’t have an account ?<Link to='/'><span>Sign up</span></Link></p>
+
+                  <h3 onClick={handleForgetPassword} className='forget-password'>Forget Password</h3>
+                </div>
               </div>
 
-            </div>
-            {
-              loader
-                ? <Bars
-                  height="80"
-                  width="80"
-                  color="#4fa94d"
-                  ariaLabel="bars-loading"
-                  wrapperStyle={{}}
-                  wrapperClass=""
-                  visible={true}
-                />
-                :
-                <MyButton onClick={handleLogin} variant="contained">Login to Continue</MyButton>
+            </Grid>
+            <Grid size={6}>
+              <img className='reg-image' src={LoginImage} alt="" />
+            </Grid>
 
-            }
-            <p>Don’t have an account ?<Link to='/'><span>Sign up</span></Link></p>
-
-          </div>
-        </div>
-
-      </Grid>
-      <Grid size={6}>
-        <img className='reg-image' src={LoginImage} alt="" />
-      </Grid>
-
-    </Grid>
+          </Grid>
+      }
+    </>
 
   )
 }
