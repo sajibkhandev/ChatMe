811afcbafbe7853641f7d89e44f7a633a85867e3
sajibkhandev <sajibkhan.mern@gmail.com>
2025-08-11 22:16:40 +0600

    add block

--- src/layouts/FriendsList.jsx
+++ src/layouts/FriendsList.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { IoSearch } from "react-icons/io5";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import SingleUser from '../components/SingleUser';
-import { getDatabase, ref, onValue, set, push } from "firebase/database";
+import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
 import { useSelector } from 'react-redux';
 
 
@@ -16,8 +16,9 @@ const FriendsList = () => {
       onValue(friendRef, (snapshot) => {
         let arr=[]
         snapshot.forEach(item=>{
-          arr.push(item.val());
-          
+          if(data.uid==item.val().receiverid || data.uid==item.val().senderid){
+            arr.push({...item.val(),key:item.key});
+          }
           
         })
         setFriend(arr)
@@ -25,6 +26,38 @@ const FriendsList = () => {
       });
   
     }, [])
+
+
+    let handleBlock=(item)=>{
+      console.log(item);
+
+
+      if(data.uid==item.senderid){
+        set(push(ref(db, 'block/')), {
+
+        block:item.receivername,
+        blockid:item.receiverid,
+        blockby:item.sendername,
+        blockbyid:item.senderid
+        }).then(()=>{
+          remove(ref(db, 'friend/' +item.key))
+        })
+      }else{
+         set(push(ref(db, 'block/')), {
+        block:item.sendername,
+        blockid:item.senderid,
+        blockby:item.receivername,
+        blockbyid:item.receiverid
+    
+        }).then(()=>{
+          remove(ref(db, 'friend/' +item.key))
+        })
+
+      }
+      
+      
+      
+    }
 
 
   return (
@@ -46,11 +79,11 @@ const FriendsList = () => {
                               <div className='title-pere'>
                                   <div className='image-box'><img src="https://firebasestorage.googleapis.com/v0/b/chatme-292ec.firebasestorage.app/o/avater.webp?alt=media&token=7dcec873-c670-4239-9326-c6a84fc24054" alt="" /></div>
                                   <div>
-                                          <h4>{item.sendername}</h4>
-                                          <p>sdfsdf</p>
+                                      <h4>{data.uid==item.receiverid ? item.sendername :item.receivername}</h4>
+                                      <p>sdfsdf</p>
                                  </div>
                               </div>
-                              <button>Block</button>
+                              <button onClick={()=>handleBlock(item)}>Block</button>
                           </div>
             ))
            }
